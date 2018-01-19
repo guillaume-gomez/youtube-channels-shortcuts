603ab8bf0f297dbe4a70c58027abd55758e6b71b
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-19 18:48:34 +0100

    externalise data

--- popup.js
+++ popup.js
@@ -1,12 +1,3 @@
-const channels = [
-  {name: "Guillaume Gomez", url: "https://www.youtube.com/signin?feature=masthead_switcher&next=%2Fdashboard%3Fo%3DU&action_handle_signin=true&authuser=0&skip_identity_prompt=False"},
-  {name: "Tom la depanneuse", url: "https://www.youtube.com/signin?pageid=103054960866031661194&authuser=0&action_handle_signin=true&next=%2F&feature=masthead_switcher&skip_identity_prompt=False"},
-  {name: "Amuse Production", url: "https://www.youtube.com/signin?content_owner=8_tcxbkqifWJhg_qQZilBg&authuser=0&action_handle_signin=true&next=%2Fdashboard%3Fo%3D8_tcxbkqifWJhg_qQZilBg&feature=masthead_switcher&skip_identity_prompt=False"},
-  {name: "Troisieme et derniere chaine", url: "https://www.youtube.com/signin?pageid=113499566998840367996&authuser=0&action_handle_signin=true&next=%2F&feature=masthead_switcher&skip_identity_prompt=False"},
-  {name: "SecondeChaine test", url: "https://www.youtube.com/signin?pageid=108822329699081727760&authuser=0&action_handle_signin=true&next=%2F&feature=masthead_switcher&skip_identity_prompt=False"},
-  {name: "Apprendre avec dino", url: "https://www.youtube.com/signin?pageid=117039495347400891925&authuser=0&action_handle_signin=true&next=%2F&feature=masthead_switcher&skip_identity_prompt=False"}
-]
-
 function fillDropdown(id = "dropdown") {
   let dropdown = document.getElementById(id);
   channels.forEach(item => {
@@ -26,6 +17,7 @@ function connectToChannel(url) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(channels)
   var dropdown = document.getElementById('dropdown');
 
   fillDropdown();
