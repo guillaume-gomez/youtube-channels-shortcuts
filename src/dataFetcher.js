2134eacde575377b72cea2e83837382706343495
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-07 01:18:28 +0100

    uppercase default actions

--- src/dataFetcher.js
+++ src/dataFetcher.js
@@ -12,11 +12,11 @@ function getCategories(callback) {
 
 function getActions(callback) {
   const defaultActions = [
-    {title: "dashboard", url: "https://www.youtube.com/dashboard?o=U", icon: "fa fa-home", enabled: true},
-    {title: "channel", url: "https://www.youtube.com/features", icon: "fa fa-television", enabled: true},
-    {title: "analytics", url: "https://www.youtube.com/analytics?o=U", icon: "fa fa-bar-chart", enabled: true},
-    {title: "video", url: "https://www.youtube.com/my_videos?o=U", icon: "fa fa-youtube-play", enabled: true},
-    {title: "playlist", url: "https://www.youtube.com/view_all_playlists", icon: "fa fa-list", enabled: true}
+    {title: "Dashboard", url: "https://www.youtube.com/dashboard?o=U", icon: "fa fa-home", enabled: true},
+    {title: "Channel", url: "https://www.youtube.com/features", icon: "fa fa-television", enabled: true},
+    {title: "Analytics", url: "https://www.youtube.com/analytics?o=U", icon: "fa fa-bar-chart", enabled: true},
+    {title: "Video", url: "https://www.youtube.com/my_videos?o=U", icon: "fa fa-youtube-play", enabled: true},
+    {title: "Playlist", url: "https://www.youtube.com/view_all_playlists", icon: "fa fa-list", enabled: true}
   ];
 
   chrome.storage.sync.get("actions", (items) => {
