4a2f1de6eb5c614075861f5fddfea44c2c52f2f6
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-13 14:14:06 +0100

    add new links

--- src/dataFetcher.js
+++ src/dataFetcher.js
@@ -16,7 +16,10 @@ function getActions(callback) {
     {title: "Channel", url: "https://www.youtube.com/features", icon: "fa fa-television", enabled: true},
     {title: "Analytics", url: "https://www.youtube.com/analytics?o=U", icon: "fa fa-bar-chart", enabled: true},
     {title: "Video", url: "https://www.youtube.com/my_videos?o=U", icon: "fa fa-youtube-play", enabled: true},
-    {title: "Playlist", url: "https://www.youtube.com/view_all_playlists", icon: "fa fa-list", enabled: true}
+    {title: "Playlist", url: "https://www.youtube.com/view_all_playlists", icon: "fa fa-list", enabled: true},
+    {title: "Comments", url: "https://www.youtube.com/comments", icon: "fa fa-comment", enabled: true},
+    {title: "Messages", url: "https://www.youtube.com/comments", icon: "fa fa-comments", enabled: true},
+    {title: "Advanced settings", url: "https://www.youtube.com/advanced_settings", icon: "fa fa-cogs", enabled: true}
   ];
 
   chrome.storage.sync.get("actions", (items) => {
